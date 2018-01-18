bdc37060afa60f39e327af069cbdca64cab7b60c
bigvlad <negreanu_vlad@yahoo.com>
2018-01-18 10:03:04 +0200

    all

--- src/main.js
+++
@@ -1,4 +0,0 @@
-import cssVar from "indexStyle.css";
-// import cssVar from "!style-loader!css-loader!./indexStyle.css";
-// import cssVar from "!css-loader!./indexStyle.css";
-// console.log(cssVar);
--- webpack.config.js
+++ webpack.config.js
@@ -2,17 +2,24 @@ var webpack=require('webpack');
 var path = require('path');
 
 module.exports={
-    entry:'./src/main.js',
+    entry:'./src/main.ts',
+    // resolve: {
+    //     extensions: [ '.tsx', '.ts', '.js' ]
+    // },
     output:{
         //path:'./dist',  //=> configuration.output.path: The provided value "./dist" is not an absolute path!
         path:path.join(__dirname,'./dist'),
         filename:'build.js'
     },
-    // module: {
-    //     loaders: [
-    //     //   { test: /\.css$/, loader: 'style-loader!css-loader' },
-    //       { test: /\.css$/, loader: ['style-loader','css-loader'] },
-    //     ]
-    // },
+    module: {
+        loaders: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+              },
+            { test: /\.css$/, loader: ['style-loader','css-loader'] },
+        ]
+    },
     //watch: true
 }
